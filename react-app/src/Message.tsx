834cbdb6052f45a7aee3e22d2232010446afc107
Tracie Childers <traciechilders01@gmail.com>
2025-04-07 09:47:43 -0400

    adding react

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,11 @@
+
+// PascalCasing
+function Message() {
+    // JSX JavaScript XML
+    const name = 'Tracie';
+    if (name)
+        return <h1>Hello There {name}</h1>;
+return  <h1>Hello There</h1>;
+}
+
+export default Message;
